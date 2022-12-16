cfd893f9a876c12648e9f97e08f1b8a588e46f80
nicocarcamo <nicolascarcamonaccarino@gmail.com>
2022-12-16 20:12:47 +0000

    little fix

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -3,15 +3,11 @@ import React, { useState } from 'react';
 const TrafficLight = () => {
   const [light, setLight] = useState('red');
 
-  const handleClick = color => {
-    setLight(color);
-  };
-
   return (
     <div className="traffic-light mt-5 d-flex">
-      <div className={`light red ${light === 'red' ? 'active' : ''}`} onClick={() => handleClick('red')}/>
-      <div className={`light yellow ${light === 'yellow' ? 'active' : ''}`} onClick={() => handleClick('yellow')}/>
-      <div className={`light green ${light === 'green' ? 'active' : ''}`} onClick={() => handleClick('green')}/>
+      <div className={`light red ${light === 'red' ? 'active' : ''}`} onClick={() => setLight('red')}/>
+      <div className={`light yellow ${light === 'yellow' ? 'active' : ''}`} onClick={() => setLight('yellow')}/>
+      <div className={`light green ${light === 'green' ? 'active' : ''}`} onClick={() => setLight('green')}/>
     </div>
   );
 };
